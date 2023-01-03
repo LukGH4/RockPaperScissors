e3ee501e3b7609cb52ec10dd36775df0c3da09c1
Luke <19198075+LukGH4@users.noreply.github.com>
2023-01-03 16:23:55 -0700

    Edit images dir and index.html and the rest

--- script.js
+++ script.js
@@ -4,54 +4,92 @@ let compScore = 0;
 let tieAmt = 0;
 
 function playRound (playerSelection, computerSelection) {
+    
 
-    console.log(`Player's Choice: ${playerSelection} 
-    Computer Choice: ${computerSelection}`)
     if (playerSelection === 'rock') {
+        playerChoice.innerHTML = `Your Choice <image src='./images/rock.png'
+            style='width: clamp(20px, 10vw, 150px); height: clamp(15px, 12vw, 120px);'>`;
         if (computerSelection === 'rock') {
+            computerChoice.innerHTML = `Computer's Choice <image src='./images/rock.png' 
+                style='width: clamp(20px, 10vw, 150px); height: clamp(15px, 12vw, 120px);'>`;
+            winSide.textContent = '=';
             tieAmt++;
             tieAmtDisplay.textContent = `Ties: ${tieAmt}`;
         }
         else if (computerSelection === 'paper') {
+            computerChoice.innerHTML = `Computer's Choice <image src='./images/paper.png'
+                style='width: clamp(20px, 10vw, 150px); height: clamp(15px, 12vw, 120px);'>`;
+            winSide.innerHTML = `<image src='./images/rightpunch.png'
+                style='width: clamp(20px, 10vw, 150px); height: clamp(15px, 12vw, 120px);'>`;
             compScore++;
             compScoreDisplay.textContent = `Computer Score: ${compScore}`;
         }
         else {
-           playerScore++;
-           playerScoreDisplay.textContent = `Player Score: ${playerScore}`;
+            computerChoice.innerHTML = `Computer's Choice <image src='./images/scissors.png'
+                style='width: clamp(20px, 10vw, 150px); height: clamp(15px, 12vw, 120px);'>`;
+            winSide.innerHTML = `<image src='./images/leftpunch.png'
+                style='width: clamp(20px, 10vw, 150px); height: clamp(15px, 12vw, 120px);'>`;
+            playerScore++;
+            playerScoreDisplay.textContent = `Player Score: ${playerScore}`;
         }
     }
     else if (playerSelection === 'paper') {
+        playerChoice.innerHTML = `Your Choice <image src='./images/paper.png'
+            style='width: clamp(20px, 10vw, 150px); height: clamp(15px, 12vw, 120px);'>`;
         if (computerSelection === 'rock') {
+            computerChoice.innerHTML = `Computer's Choice <image src='./images/rock.png' 
+                style='width: clamp(20px, 10vw, 150px); height: clamp(15px, 12vw, 120px);'>`;
+            winSide.innerHTML = `<image src='./images/leftpunch.png'
+                style='width: clamp(20px, 10vw, 150px); height: clamp(15px, 12vw, 120px);'>`;
             playerScore++;
             playerScoreDisplay.textContent = `Player Score: ${playerScore}`;
         }
         else if (computerSelection === 'paper') {
+            computerChoice.innerHTML = `Computer's Choice <image src='./images/paper.png'
+                style='width: clamp(20px, 10vw, 150px); height: clamp(15px, 12vw, 120px);'>`;
+            winSide.textContent = '=';
             tieAmt++;
             tieAmtDisplay.textContent = `Ties: ${tieAmt}`;
         }
         else {
+            computerChoice.innerHTML = `Computer's Choice <image src='./images/scissors.png' 
+                style='width: clamp(20px, 10vw, 150px); height: clamp(15px, 12vw, 120px);'>`;
+            winSide.innerHTML = `<image src='./images/rightpunch.png'
+                style='width: clamp(20px, 10vw, 150px); height: clamp(15px, 12vw, 120px);'>`;
             compScore++;
             compScoreDisplay.textContent = `Computer Score: ${compScore}`;
         }
     }
     else if (playerSelection === 'scissors') {
+        playerChoice.innerHTML = `Your Choice <image src='./images/scissors.png'
+                style='width: clamp(20px, 10vw, 150px); height: clamp(15px, 12vw, 120px);'>`;
         if (computerSelection === 'rock') {
+            computerChoice.innerHTML = `Computer's Choice <image src='./images/rock.png' 
+                style='width: clamp(20px, 10vw, 150px); height: clamp(15px, 12vw, 120px);'>`;
+            winSide.innerHTML = `<image src='./images/rightpunch.png'
+                style='width: clamp(20px, 10vw, 150px); height: clamp(15px, 12vw, 120px);'>`;
             compScore++;
             compScoreDisplay.textContent = `Computer Score: ${compScore}`;
         }
         else if (computerSelection === 'paper') {
+            computerChoice.innerHTML = `Computer's Choice <image src='./images/paper.png'
+                style='width: clamp(20px, 10vw, 150px); height: clamp(15px, 12vw, 120px);'>`;
+            winSide.innerHTML = `<image src='./images/leftpunch.png'
+                style='width: clamp(20px, 10vw, 150px); height: clamp(15px, 12vw, 120px);'>`;
             playerScore++;
             playerScoreDisplay.textContent = `Player Score: ${playerScore}`;
         }
         else {
+            computerChoice.innerHTML = `Computer's Choice <image src='./images/scissors.png'
+                style='width: clamp(20px, 10vw, 150px); height: clamp(15px, 12vw, 120px);'>`;
+            winSide.textContent = '=';
             tieAmt++;
             tieAmtDisplay.textContent = `Ties: ${tieAmt}`;
         }
     }
 
     if (playerScore === 5) {
-        finalDisplay.textContent = 'You won the game!';
+        finalDisplay.textContent = 'Final: You won the game!';
         tieAmt = 0;
         playerScore = 0;
         compScore = 0;
@@ -61,7 +99,7 @@ function playRound (playerSelection, computerSelection) {
 
     }
     else if (compScore === 5) {
-        finalDisplay.textContent = 'You lost the game!';
+        finalDisplay.textContent = 'Final: You lost the game!';
         tieAmt = 0;
         playerScore = 0;
         compScore = 0;
@@ -90,6 +128,10 @@ function scissorsChoice() {
 const rock = document.querySelector('.rock');
 const paper = document.querySelector('.paper');
 const scissors = document.querySelector('.scissors');
+
+const playerChoice = document.querySelector('.player-choice')
+const computerChoice = document.querySelector('.computer-choice')
+const winSide = document.querySelector('.win-side');
 
 rock.addEventListener('click', rockChoice);
 paper.addEventListener('click', paperChoice);
